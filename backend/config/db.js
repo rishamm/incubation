6d898cfe39d3c9d4882adf265238a1eaa9b7163c
rishamm <muhammedrisham45334@gmail.com>
2022-11-17 15:05:58 +0530

    scnd commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/Incubation',{
+mongoose.connect('mongodb+srv://rish:rish123@incubation.ufgyi6z.mongodb.net/test',{
     useNewUrlParser:true,
    
 }).then(()=>{
     console.log('db connected');
 }).catch((e)=>{
+    console.log(e)
     console.log('db not connected');
 })
